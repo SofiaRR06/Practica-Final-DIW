1e31651a7846de1468f197ecab63341263dfb564
SofiaRR06 <alumno.708095@ies-azarquiel.es>
2026-02-03 13:13:03 +0100

    metodologia 2

--- src/components/Methodologies.tsx
+++ src/components/Methodologies.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import  Kanban  from "../assets/metodologia-kanban.jpg";
+import  Kanban  from "../assets/metodologia-kanban.png";
 import Scrum from "../assets/metodologia-scrum.jpg";
 
 const Methodologies: React.FC = () => {
   return (
     <div className="bg-white mx-auto px-4 py-10 mt-20 rounded-2xl" >
-      <h2 className="text-4xl font-bold text-center  text-blue">
+      <h2 className="text-5xl font-bold text-center  text-blue">
         Metodologías que usamos
       </h2>
 
       <div className="flex flex-col md:flex-row items-center justify-between gap-8">
         {/* Cuadro izquierdo */}
-        <div className="w-100 h-100 flex items-center justify-center">
+        <div className="w-90 h-90 flex items-center justify-center">
           <img src={Kanban} className="rounded-md"/>
         </div>
 
@@ -26,7 +26,7 @@ const Methodologies: React.FC = () => {
         </div>
 
         {/* Cuadro derecho */}
-        <div className="w-40 h-40 bg-gray-300 flex items-center justify-center rounded-md">
+        <div className="w-90 h-90 flex items-center justify-center rounded-md">
                     <img src={Scrum} />
         </div>
       </div>
